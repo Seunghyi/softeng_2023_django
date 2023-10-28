2c301d4c11e2d20fe0ee0361e7cfe136042a5f63
KangSeunghee <rkdtmdgml010@jbnu.ac.kr>
2023-10-28 22:13:28 +0900

    과제 #12

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
+#def index(request):
+#    posts = Post.objects.all().order_by('-updated_at')
+#
+#    return render(
+#        request,
+#        "blog/post_list.html",
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+#def single_post_page(request, pkey):
+#    post = Post.objects.get(pk=pkey)
+#    return render(
+#        request,
+#        "blog/post_detail.html",
+#        {'post': post},
+#    )
