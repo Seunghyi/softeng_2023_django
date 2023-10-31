fdf0ce430a4d193627e8eb1feded6e079aea0100
KangSeunghee <rkdtmdgml010@jbnu.ac.kr>
2023-11-01 00:49:21 +0900

    과제 #13

--- softeng_2023_prj/urls.py
+++ softeng_2023_prj/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', include('single_pages.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
