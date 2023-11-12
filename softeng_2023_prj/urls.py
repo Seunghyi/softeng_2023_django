9969ac1bc15605a664486474b6000fcc657fa0b5
KangSeunghee <rkdtmdgml010@jbnu.ac.kr>
2023-11-12 13:59:42 +0900

    과제 #16

--- softeng_2023_prj/urls.py
+++ softeng_2023_prj/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -27,7 +28,7 @@ urlpatterns = [
     path("todo/", include("todo_app.urls"))
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns() 
- 
+    urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
